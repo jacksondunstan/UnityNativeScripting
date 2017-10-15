2d10f5dac0373db3690803022f749bd71d22d4ff
Jackson Dunstan <jackson@jacksondunstan.com>
2017-10-15 12:28:14 -0700

    Remove delegate test code

--- Unity/CppSource/Game/Game.cpp
+++ Unity/CppSource/Game/Game.cpp
@@ -16,96 +16,12 @@ using namespace UnityEngine;
 
 void PrintPlatformDefines();
 
-struct PlainAction : System::Action
-{
-	void operator()() override
-	{
-		Debug::Log(String("PlainAction invoked"));
-	}
-};
-
-struct FloatAction : System::Action1<float>
-{
-	void operator()(float param) override
-	{
-		Debug::Log(String("FloatAction invoked"));
-	}
-};
-
-struct MyClickHandler : System::Action2<float, float>
-{
-	void operator()(float x, float y) override
-	{
-		Debug::Log(String("clicked"));
-	}
-};
-
-struct MyIntFloatDoubleFunc : System::Func3<int32_t, float, double>
-{
-	double operator()(int32_t i, float f) override
-	{
-		Debug::Log(String("int float double Func invoked"));
-		return 2.34;
-	}
-};
-
-struct FuncReturningString : System::Func3<int16_t, int32_t, String>
-{
-	String operator()(int16_t s, int32_t i) override
-	{
-		Debug::Log(String("returning a string"));
-		return String("returned from Func");
-	}
-};
-
-struct MyAppDomainInitializer : AppDomainInitializer
-{
-	void operator()(System::Array1<System::String> args) override
-	{
-		for (int i = 0, len = args.GetLength(); i < len; ++i)
-		{
-			Debug::Log(args.GetItem(i));
-		}
-	}
-};
-
 // Called when the plugin is initialized
 // This is mostly full of test code. Feel free to remove it all.
 void PluginMain()
 {
 	PrintPlatformDefines();
 	Debug::Log(String("Game booted up"));
-	
-	MyClickHandler mch1;
-	MyClickHandler mch2;
-	mch1 += mch2;
-	mch1.Invoke(123, 456);
-	Debug::Log(String("Removed"));
-	mch1 -= mch2;
-	mch1.Invoke(123, 456);
-	
-	MyIntFloatDoubleFunc mifdf;
-	double d = mifdf.Invoke(123, 3.14f);
-	char buf[1024];
-	sprintf(buf, "%lf", d);
-	Debug::Log(String(buf));
-	
-	FuncReturningString frs;
-	String str = frs.Invoke(11, 22);
-	Debug::Log(str);
-	
-	FloatAction fa;
-	fa.Invoke(3.14f);
-	
-	PlainAction pa;
-	pa.Invoke();
-	
-	MyAppDomainInitializer madi;
-	AppDomainSetup ads;
-	ads.SetAppDomainInitializer(madi);
-	Array1<String> invokeParams(1);
-	invokeParams.SetItem(0, "Hello");
-	ads.GetAppDomainInitializer().Invoke(invokeParams);
 	
 	GameObject go(String("GameObject with a TestScript"));
 	go.AddComponent<MyGame::MonoBehaviours::TestScript>();
